a32dd6e282f371f9d6ed9d7885949458dcd49bd2
Developer <developer@Developers-Mac.local>
2018-06-04 14:31:37 +0200

    Initial Commit

---
+++ RickNMortyShowCase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RickNMortyShowCase
+//
+//  Created by Developer on 04/06/2018.
+//  Copyright © 2018 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
